4814ce848fe189b7b848cf2c0aeb62cab586c020
steffejr <steffener@gmail.com>
2015-05-10 11:43:10 -0400

    Initial create

---
+++ ExpDesign.m
@@ -0,0 +1,7 @@
+% Edith experiment
+% Time clock
+% Each trial is composed of watching a five second video.
+% There are two types of videos
+% action videos
+% broken into two categories
+% scrambled videos
