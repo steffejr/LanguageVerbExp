182e88ba4c3017dd5cc9247a28bf6cb03cf6030a
steffejr <steffener@gmail.com>
2015-05-10 12:10:05 -0400

    writing out notes

--- ExpDesign.m
+++ ExpDesign.m
@@ -1,7 +1,41 @@
-% Edith experiment
-% Time clock
+
+% Time clock start: 11:40AM 5/10
 % Each trial is composed of watching a five second video.
 % There are two types of videos
 % action videos
-% broken into two categories
-% scrambled videos
+% broken into categories
+    % videos correctly answered at initial view (1)
+        % answered correctly (1a)
+        % answered incorrectly (1b)
+    % videos incorrectly answered at initial view (2) 
+        % studied (2a)
+            % answered correctly (2a1)
+            % answered incorrectly (2a2)
+        % not studied (2b)
+            % answered correctly (2b1)
+            % answered incorrectly (2b2)
+% scrambled videos (3)
+
+% Two time points
+    % before training
+        % (A1)
+            % (A1a)
+            % (A1b)
+        % (A2)
+        % (A3) 
+    % after training
+        % (B1)
+            % (B1a)
+            % (B1b)
+        % (B2a)
+            % (B2a1)
+            % (B2a2)
+        % (B2b)
+            % (B2b1)
+            % (B2b2)
+        % (B3)
+% Hypothesis
+    % Does brain activation in studied items change as a function of
+    % training?
+        % B2a - A2
+        % 
